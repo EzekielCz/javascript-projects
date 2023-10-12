73f6107451954acbb3b83b5e266bd62bddc5eb9b
EzekielCz <ezechielcazacu@gmail.com>
2023-10-12 17:37:03 -0500

    commit

--- unit-testing/chapter-examples/transmission-processor/tests/processor.test.js
+++ unit-testing/chapter-examples/transmission-processor/tests/processor.test.js
@@ -5,5 +5,29 @@ describe("transmission processor", function() {
     let result = processor("9701::<489584872710>");
          expect(typeof result).toBe("object");
   });
- 
+  test("returns -1 if '::' not found", function() {
+    let result = processor("9701<489584872710>");
+    expect(result).toBe(-1);
+ });
+ test("returns id in object", function() {
+  let result = processor("9701::<489584872710>");
+  expect(result.id).not.toBeUndefined();
+});
+test("converts id to a number", function() {
+  let result = processor("9701::<489584872710>");
+  expect(result.id).toBe(9701);
+});
+test("returns rawData in object", () => {
+  let result = processor("9701::<487297403495720912>");
+  expect(result.rawData).not.toBeUndefined();
+});
+test("returns -1 for rawData if missing < at position 0", function() {
+  let result = processor("9701::487297403495720912>");
+  expect(result.rawData).toBe(-1);
+});
+test("Trim leading and trailing whitespace from transmission", function(){
+  let result = processor("transmission");
+  expect(result).toBe("transmission");
+});
+
  });
--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,10 @@
 
 let launchcode = {
-  
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    method: launchOutput()
 }
 
 module.exports = launchcode;
