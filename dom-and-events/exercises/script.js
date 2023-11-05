c4df49ae37044d608db3d97a2b36d7ce125cba35
EzekielCz <ezechielcazacu@gmail.com>
2023-11-04 23:05:13 -0500

    DOM and Events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,22 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    // const abortMission = document.getElementById("abortMission");
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+        missionAbort.addEventListener("mouseover", function () {
+        abortMission.style.backgroundColor = "red";
+     });
+     missionAbort.addEventListener("mouseout", function() {
+        missionAbort.style.backgroundColor = "";
+     });
+     missionAbort.addEventListener("click",function(){
+        let response = window.confirm("Are you sure you want to abort the mission?");
+     if (response === true){
+        window.alert("Mission aborted! Space shuttle returning home");
+     };
+     });
 }
 
 window.addEventListener("load", init);
